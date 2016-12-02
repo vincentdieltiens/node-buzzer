a74cd00c09f515a69be60b9f83b8a9abd17aab72
Vincent Dieltiens <and1hotsauce@gmail.com>
2016-12-02 23:03:39 +0100

    Add WebsocketBuzzer and use it in WebBuzzer

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export { WebBuzzer } from './web/buzzer';
 export { GPIOBuzzer, GPIODomePushButton } from './gpio/buzzer'; 
 export { Ps2Buzzer } from './ps2/buzzer'; 
 export { TeensyBuzzer } from './teensy/buzzer'; 
+export { WebsocketBuzzer } from './websocket/buzzer'; 
---
+++ websocket/index.ts
@@ -0,0 +1,1 @@
+export { WebsocketBuzzer } from './buzzer'
